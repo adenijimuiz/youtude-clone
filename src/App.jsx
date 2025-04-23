c82f55f4a10e087fb815ce41c8cb83d8d8efb952
adenijimuiz <muizodunayo111@gmail.com>
2025-04-23 19:07:31 +0100

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './Components/Navbar/Navbar'
+import { Sidebar } from './Components/Sidebar/Sidebar'
+import { Home } from './pages/Home/Home'
+import { Video } from './pages/Video/Video'
 
 function App() {
 
 
   return (
     <>
-      <h1>app</h1>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
+      </Routes>
     </>
   )
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import './Sidebar.css'
+import home from '../../assets/home.png'
+import game_icon from '../../assets/game_icon.png'
+import automobiles from '../../assets/automobiles.png'
+import sports from '../../assets/sports.png'
+import entertainment from '../../assets/entertainment.png'
+import tech from '../../assets/tech.png'
+import music from '../../assets/music.png'
+import blogs from '../../assets/blogs.png'
+import news from '../../assets/news.png'
+import jack from '../../assets/jack.png'
+import simon from '../../assets/simon.png'
+import tom from '../../assets/tom.png'
+import megan from '../../assets/megan.png'
+import cameron from '../../assets/cameron.png'
+
+export const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="shortcut-links">
+        <div className="side-link">
+          <img src={home} alt="home" />
+          <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={game_icon} alt="gaming" />
+          <p>Gaming</p>
+        </div>
+        <div className="side-link">
+          <img src={automobiles} alt="Automobiles" />
+          <p>Automobiles</p>
+        </div>
+        <div className="side-link">
+          <img src={sports} alt="sport" />
+          <p>Sport</p>
+        </div>
+        <div className="side-link">
+          <img src={entertainment} alt="Entertainment" />
+          <p>Entertainment</p>
+        </div>
+        <div className="side-link">
+          <img src={tech} alt="tech" />
+          <p>Technology</p>
+        </div>
+        <div className="side-link">
+          <img src={music} alt="muisc" />
+          <p>Music</p>
+        </div>
+        <div className="side-link">
+          <img src={blogs} alt="blogs" />
+          <p>Blogs</p>
+        </div>
+        <div className="side-link">
+          <img src={news} alt="news" />
+          <p>News</p>
+        </div>
+
+        <hr />
+      </div>
+
+      <div className="subscribed-list">
+          <h3>Subscribed</h3>
+          <div className="side-link">
+            <img src={jack} alt="jack" />
+            <p>PewDiePie</p>
+          </div>
+          <div className="side-link">
+            <img src={simon} alt="simon" />
+            <p>MrBeast</p>
+          </div>
+          <div className="side-link">
+            <img src={tom} alt="tom" />
+            <p>Tom Tom</p>
+          </div>
+          <div className="side-link">
+            <img src={megan} alt="megan" />
+            <p>5-Minute Crafts</p>
+          </div>
+          <div className="side-link">
+            <img src={cameron} alt="cameron" />
+            <p>Nas Daily</p>
+          </div>
+        </div>
+    </div>
+  )
+}
